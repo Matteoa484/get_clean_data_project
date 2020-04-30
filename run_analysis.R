2101974786b208a9eb0dc93ca494d0f5476d5909
Mat notebook <matteo.anro@gmail.com>
2020-04-30 19:34:52 +0200

    changes in read_table

--- run_analysis.R
+++ run_analysis.R
@@ -2,29 +2,41 @@ library(tidyverse)
 
 # upload raw data
 
-features <- read_table("./UCI HAR Dataset/features.txt", col_names = FALSE) 
+features <- 
+    read_table(
+      "./UCI HAR Dataset/features.txt", 
+      col_names = c("features_list")
+    ) %>%
+    pull(features_list) %>%
+    str_replace("^[0-9] |[0-9][0-9] |[0-9][0-9][0-9] ", "")         
+    
+activity_labels <- 
+  read_table(
+    "./UCI HAR Dataset/activity_labels.txt", 
+    col_names = c("activity_id", "activity"), 
+    col_types = cols(activity_id = col_integer(), activity = col_character())
+  ) %>%
+  mutate(activity = tolower(activity))
+
+train_set <- 
+    read_table(
+        "./UCI HAR Dataset/train/X_train.txt", 
+        col_names = FALSE
+      )
+
+
 test_set <- read_table("./UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
 test_labels <- read_table("./UCI HAR Dataset/test/y_test.txt", col_names = FALSE, col_types = "i")
-train_set <- read_table("./UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
+
 train_labels <- read_table("./UCI HAR Dataset/train/y_train.txt", col_names = FALSE, col_types = "i")
 
-activity_labels <- 
-    read_table(
-        "./UCI HAR Dataset/activity_labels.txt", 
-          col_names = FALSE, 
-          col_types = cols(X1 = col_integer(), X2 = col_character())
-    )
 
 # prepare variable and activity names
 
-features <- 
-    features$X1 %>% 
+features_x <- 
+    features$features_list %>% 
     str_replace("^[0-9] |[0-9][0-9] |[0-9][0-9][0-9] ", "")
 
-activity_labels <- 
-    activity_labels %>%
-    rename(activity_id = X1, activity = X2) %>%
-    mutate(activity = tolower(activity))
 
 full_labels <- train_labels %>% bind_rows(test_labels)
 
@@ -47,7 +59,7 @@ full_set <-
 
 
 
-rm("features", "test_set", "train_set")
+rm("features", "test_set", "train_set", "train_labels", "test_labels")
 
 # bind_cols to merge columns (cbind equivalent)
 # x test df, y test lista attivita, da aggiungere come colonna e mettere in relazione con activity labels
