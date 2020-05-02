01756bd228a67f5e410a321a6c285b1d680b7e24
Mat notebook <matteo.anro@gmail.com>
2020-05-02 16:32:18 +0200

    wroking script. still need to change variable names

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,13 @@
 library(tidyverse)
 
-# upload raw data
+# 1. upload raw data
 
+# variable labels
 features <- 
-    read_table(
-      "./UCI HAR Dataset/features.txt", 
-      col_names = c("features_list")
-    ) %>%
-    pull(features_list) %>%
-    str_replace("^[0-9] |[0-9][0-9] |[0-9][0-9][0-9] ", "")         
-    
+    read_table("./UCI HAR Dataset/features.txt", col_names = c("features_list")) %>%
+    pull(features_list)     
+
+# activity labels
 activity_labels <- 
   read_table(
     "./UCI HAR Dataset/activity_labels.txt", 
@@ -18,49 +16,54 @@ activity_labels <-
   ) %>%
   mutate(activity = tolower(activity))
 
-train_set <- 
-    read_table(
-        "./UCI HAR Dataset/train/X_train.txt", 
-        col_names = FALSE
-      )
-
-
+# set data
+train_set <- read_table("./UCI HAR Dataset/train/X_train.txt",  col_names = FALSE)
 test_set <- read_table("./UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
-test_labels <- read_table("./UCI HAR Dataset/test/y_test.txt", col_names = FALSE, col_types = "i")
+full_set <- train_set %>% bind_rows(test_set)
+rm("train_set", "test_set")
 
-train_labels <- read_table("./UCI HAR Dataset/train/y_train.txt", col_names = FALSE, col_types = "i")
-
-
-# prepare variable and activity names
-
-features_x <- 
-    features$features_list %>% 
-    str_replace("^[0-9] |[0-9][0-9] |[0-9][0-9][0-9] ", "")
-
-
+# labels data
+train_labels <- read_table("./UCI HAR Dataset/train/y_train.txt", 
+                           col_names = c("activity_id"), 
+                           col_types = "i"
+                )
+test_labels <- read_table("./UCI HAR Dataset/test/y_test.txt", 
+                          col_names = c("activity_id"), 
+                          col_types = "i"
+                )
 full_labels <- train_labels %>% bind_rows(test_labels)
+rm("train_labels", "test_labels")
 
-# create merged data frame
+# subject data
+train_subject <- read_table("./UCI HAR Dataset/train/subject_train.txt", 
+                            col_names = c("subject"), 
+                            col_types = "i")
+test_subject <- read_table("./UCI HAR Dataset/test/subject_test.txt", 
+                          col_names = c("subject"), 
+                          col_types = "i")
+full_subject <- train_subject %>% bind_rows(test_subject)
+rm("train_subject", "test_subject")
+
+
+# 2. create merged data frame and extract mean/std 
 
 full_set <-
-  train_set %>%
+  full_set %>%
   magrittr::set_colnames(features) %>%    # set new variables name
-  bind_rows(test_set) %>%   # merge train and test sets
   select(matches("[Mm]ean|[Ss]td")) %>%   # select mean and std columns
-  bind_cols(full_labels) %>%    # merge labels column
-  rename(activity_id = X1) %>%    # rename new column
+  bind_cols(full_labels, full_subject) %>%    # merge labels column
   left_join(
       activity_labels, 
       by = c("activity_id" = "activity_id")
-    ) %>%
+  ) %>%
   select(-activity_id) %>%
-  select(activity, everything())
+  select(subject, activity, everything())
+
+new_set <- 
+    full_set %>% 
+    group_by(subject, activity) %>% 
+    summarise_all(mean, na.rm = TRUE)
 
 
 
-
-rm("features", "test_set", "train_set", "train_labels", "test_labels")
-
-# bind_cols to merge columns (cbind equivalent)
-# x test df, y test lista attivita, da aggiungere come colonna e mettere in relazione con activity labels
-# features dovrebbero essere nomi variabili del dataframe finale (nr = a nr colonne in df)
+# activity labels come factors, con ordine da file txt
