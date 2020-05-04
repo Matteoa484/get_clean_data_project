968775a52ad0f689e91c5d3374de0959a54b749c
Mat notebook <matteo.anro@gmail.com>
2020-05-04 19:40:11 +0200

    code flow changes

--- run_analysis.R
+++ run_analysis.R
@@ -2,13 +2,67 @@ library(tidyverse)
 
 # upload raw data ---------------------------------------------------------
 
+feat_list <- read_table("./UCI HAR Dataset/features.txt", col_names = c("features"), col_types = "c") 
 
-# column labels
-features <-
-    read_table(
-        "./UCI HAR Dataset/features.txt",
-        col_names = c("features")
-    ) %>%
+activity_labels <- 
+  read_table(
+    "./UCI HAR Dataset/activity_labels.txt", 
+    col_names = c("activity_id", "activity"), 
+    col_types = cols(activity_id = col_integer(), activity = col_character())
+  ) %>%
+  mutate(activity = tolower(activity))
+
+# set data
+data_train <- read_table("./UCI HAR Dataset/train/X_train.txt",  col_names = c(feat_list$features))
+data_test <- read_table("./UCI HAR Dataset/test/X_test.txt", col_names = c(feat_list$features))
+
+# labels data
+train_labels <- 
+  read_table(
+    "./UCI HAR Dataset/train/y_train.txt", 
+    col_names = c("activity_id"), 
+    col_types = "i"
+  )
+
+test_labels <- 
+  read_table(
+    "./UCI HAR Dataset/test/y_test.txt", 
+    col_names = c("activity_id"), 
+    col_types = "i"
+  )
+
+# subjects data
+train_subject <- 
+  read_table(
+    "./UCI HAR Dataset/train/subject_train.txt", 
+    col_names = c("subject"), 
+    col_types = "i"
+  )
+
+test_subject <- 
+  read_table(
+    "./UCI HAR Dataset/test/subject_test.txt", 
+    col_names = c("subject"), 
+    col_types = "i"
+  )
+
+
+# merge data sets ---------------------------------------------------------
+
+data_train <- data_train %>% bind_rows(data_test)
+train_labels <- train_labels %>% bind_rows(test_labels)
+train_subject <- train_subject %>% bind_rows(test_subject)
+
+full_set <-
+  data_train %>%
+  bind_cols(train_labels, train_subject) %>%
+  left_join(
+    activity_labels, 
+    by = c("activity_id" = "activity_id")
+  )
+
+
+#%>%
     pull(features) %>%
     str_to_lower() %>%
     str_replace_all(features, c(
@@ -23,52 +77,14 @@ features <-
 # str_subset(features, ".Body[a-zA-Z]{3}[:punct:]mean[:punct:]{3}X$")
 
 # activity labels
-activity_labels <- 
-    read_table(
-        "./UCI HAR Dataset/activity_labels.txt", 
-        col_names = c("activity_id", "activity"), 
-        col_types = cols(activity_id = col_integer(), activity = col_character())
-    ) %>%
-    mutate(activity = tolower(activity))
 
-# set data
-train_set <- read_table("./UCI HAR Dataset/train/X_train.txt",  col_names = FALSE)
-test_set <- read_table("./UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
-full_set <- train_set %>% bind_rows(test_set) # merge train / test sets
 
-# labels data
-train_labels <- 
-    read_table(
-        "./UCI HAR Dataset/train/y_train.txt", 
-        col_names = c("activity_id"), 
-        col_types = "i"
-    )
 
-test_labels <- 
-    read_table(
-        "./UCI HAR Dataset/test/y_test.txt", 
-        col_names = c("activity_id"), 
-        col_types = "i"
-    )
 
-full_labels <- train_labels %>% bind_rows(test_labels) # merge labels sets
 
-# subjects data
-train_subject <- 
-    read_table(
-        "./UCI HAR Dataset/train/subject_train.txt", 
-        col_names = c("subject"), 
-        col_types = "i"
-    )
 
-test_subject <- 
-    read_table(
-        "./UCI HAR Dataset/test/subject_test.txt", 
-        col_names = c("subject"), 
-        col_types = "i"
-    )
 
-full_subject <- train_subject %>% bind_rows(test_subject) # merge subjects sets
+
 
 # create merged set and extract mean/std ----------------------------------
 
